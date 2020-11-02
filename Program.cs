88676308803615ced92639fb39e30cfc09bc5c8a
Macradon <nguyen.tri0c@gmail.com>
2020-11-02 11:14:34 +0100

    Initial commit Hello World
    Initial commit to repos, base project Hello World committed

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace I6SWD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
